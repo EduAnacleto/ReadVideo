6b7af5055543b2bc07bf77c6598f6749c115b3d3
EduAnacleto <eduardo.statistic@gmail.com>
2022-07-16 12:46:26 -0300

    Read and write videos

--- read.py
+++ read.py
@@ -1,28 +1,45 @@
 import cv2 as cv
 import time
 import sys
+import os
 
 
 class ReadVideoFile:
-
-    def __init__(self, files):
-        self.files = files
+    def __init__(self, directory_path = os.path.abspath(os.getcwd())):
+        self.directory_path = directory_path
+        #self.extensions = ['.dvi', '.mp4']
+        self.extensions = ['.dvi']
+        self.checkDiractory()
+        self.filtered_files = []
 
     def __del__(self):
-        self.files.clear()
+        self.filtered_files.clear()
+
+    def createDirectory(self, folder = 'MP4Videos'):
+        isExist = os.path.exists(self.directory_path + '/' + folder)
+        if not isExist:
+            os.makedirs(self.directory_path + '/' + folder)
+
+    def checkDiractory(self):
+        if os.path.isdir(self.directory_path) == False:
+            print('The specified directory does not exist.')
+            exit()
+
+    def getFilteredFiles(self):
+        self.filtered_files = list()
+        for fname in os.listdir(self.directory_path):
+            if fname[-4:] in self.extensions:
+                self.filtered_files.append(fname)                
+        self.filtered_files.sort()
 
 
     def play(self):
-        for fname in self.files:
-            cap = cv.VideoCapture(fname)
-            print(fname)
-            if not cap.isOpened():
-                print("Error opening video file")
-                exit(0)
+        self.getFilteredFiles()
 
-            print("Video properties")
-            print("Video Dimensions: ", cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT) )
-            print("Video FPS: ", cap.get(cv.CAP_PROP_FPS) )
+        for fname in self.filtered_files:
+            cap = cv.VideoCapture(fname)
+            if not cap.isOpened():
+                continue
 
             while( cap.isOpened() ):
                 ret, img = cap.read()
@@ -36,8 +53,61 @@ class ReadVideoFile:
                 time.sleep( 1 / cap.get(cv.CAP_PROP_FPS) )
 
             cap.release()
-         
+        cv.destroyAllWindows()
+
+
+    def convertToMP4(self):
+        folder = 'MP4Videos'
+        self.createDirectory( folder=folder )
+        self.getFilteredFiles()
+
+        for fname in self.filtered_files:
+            cap = cv.VideoCapture(fname)
+            if not cap.isOpened():
+                continue
+            
+            filename = fname.replace(fname[-4:],'.mp4')
+            fourcc = cv.VideoWriter_fourcc(*'mp4v')
+            #fourcc = cv.VideoWriter_fourcc('M','P','4','V')
+            fps = 29.0
+            resolution = (640, 480)
+            videoOutPut = cv.VideoWriter(
+                    self.directory_path + '/' + folder + '/' + filename, 
+                    fourcc, 
+                    fps, 
+                    resolution)
+            
+            while( cap.isOpened() ):
+                ret, frame = cap.read()
+                if ret == True:
+                    #imgin = frame[:, :, (0, 1, 2)]
+
+                    videoOutPut.write(frame)
+
+                    cv.imshow( "camOut", frame )
+                    cv.waitKey( 1 )
+                    time.sleep( 1 / cap.get(cv.CAP_PROP_FPS) )
+
+                    if cv.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+                else:
+                    break
+
+            cap.release()
+            videoOutPut.release()
+
+        cv.destroyAllWindows()
+
+        
 
 if __name__ == '__main__':
-    rvf = ReadVideoFile(sys.argv[1:])
-    rvf.play()
+
+    path = ''
+    if len(sys.argv) == 1:
+        path = os.path.abspath(os.getcwd())
+    else:
+        path = sys.argv[1]
+
+    rvf = ReadVideoFile(path)
+    rvf.convertToMP4()
